7f4dcc7899b14d8fed5c5ccbec48fa910086e99d
Rongyi Chen <kenchenrong@gmail.com>
2022-08-17 14:32:41 -0700

    torznab indexer model

--- hamstery/admin.py
+++ hamstery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import  TorznabIndexer
 
 # Register your models here.
+
+@admin.register(TorznabIndexer)
+class TorznabIndexerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url', 'apikey']
--- hamstery/models.py
+++ hamstery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class TorznabIndexer(models.Model):
+    name = models.CharField(max_length=120)
+    url = models.CharField(max_length=1024)
+    apikey = models.CharField(max_length=128)
