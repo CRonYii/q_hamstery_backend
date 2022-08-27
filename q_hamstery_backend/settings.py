5d20e0d464cf665848c7033ba09c5a4656056fd4
Rongyi Chen <kenchenrong@gmail.com>
2022-08-27 13:31:06 -0700

    Add static root

--- q_hamstery_backend/settings.py
+++ q_hamstery_backend/settings.py
@@ -27,6 +27,7 @@ environ.Env.read_env('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / "static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
