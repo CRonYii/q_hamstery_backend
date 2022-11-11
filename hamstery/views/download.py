242023cf4838c604f2848a736b80ba45e77db4cb
Rongyi Chen <kenchenrong@gmail.com>
2022-11-11 13:23:43 -0800

    support cancel download

--- hamstery/views/download.py
+++ hamstery/views/download.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from ..models import TvDownload
@@ -25,6 +25,14 @@ class TvDownloadView(viewsets.GenericViewSet):
         serializer = self.get_serializer(instance)
         data = self.append_extra_info([serializer.data])[0]
         return Response(data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.cancel()
 
     def append_extra_info(self, download):
         hash = '|'.join(map(lambda d: d['hash'], download))
