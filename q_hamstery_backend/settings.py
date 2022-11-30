b64e6eea680ce3cf789a509c74a55007f247fe0b
Rongyi Chen <kenchenrong@gmail.com>
2022-11-30 01:24:40 +0000

    allow localhost as well

--- q_hamstery_backend/settings.py
+++ q_hamstery_backend/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env('HOST')]
+ALLOWED_HOSTS = ['127.0.0.1', env('HOST')]
 if env('CSRF_TRUSTED_ORIGINS') is not None:
     CSRF_TRUSTED_ORIGINS = [env('CSRF_TRUSTED_ORIGINS')]
 
