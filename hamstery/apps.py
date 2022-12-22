91286098b17f2a45ff88defcecde2e3b3f059fe0
Rongyi Chen <kenchenrong@gmail.com>
2022-12-22 11:53:38 -0800

    move print to logging

--- hamstery/apps.py
+++ hamstery/apps.py
@@ -1,13 +1,16 @@
+import logging
+
 import tzlocal
 from django.apps import AppConfig
 
+logger = logging.getLogger(__name__)
 
 class HamsteryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hamstery'
 
     def ready(self) -> None:
-        print('Timezone: %s' % (tzlocal.get_localzone()))
+        logger.info('Timezone: %s' % (tzlocal.get_localzone()))
         from django.conf import settings
         if settings.BUILDING is False:
             from . import background_scheduler
