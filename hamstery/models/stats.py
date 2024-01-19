a9a3754a912ee67aee59012a428f28c12a406a5c
Rongyi Chen <kenchenrong@gmail.com>
2024-01-19 14:19:24 -0800

    Fix stats race condition

--- hamstery/models/stats.py
+++ hamstery/models/stats.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 
 from hamstery.models.singleton import SingletonModel
 
@@ -11,12 +12,11 @@ class HamsteryStats(SingletonModel):
     openai_title_parser_total_tokens_used = models.IntegerField(default=0)
 
     def update_title_parser_stats(self, calls=0, fails=0, prompt_tokens=0, completion_tokens=0, total_tokens=0):
-        self.refresh_from_db()
-        self.openai_title_parser_calls += calls
-        self.openai_title_parser_failures += fails
-        self.openai_title_parser_prompt_tokens_used += prompt_tokens
-        self.openai_title_parser_completion_tokens_used += completion_tokens
-        self.openai_title_parser_total_tokens_used += total_tokens
+        self.openai_title_parser_calls = F('openai_title_parser_calls') + calls
+        self.openai_title_parser_failures = F('openai_title_parser_failures') + fails
+        self.openai_title_parser_prompt_tokens_used = F('openai_title_parser_prompt_tokens_used') + prompt_tokens
+        self.openai_title_parser_completion_tokens_used = F('openai_title_parser_completion_tokens_used') + completion_tokens
+        self.openai_title_parser_total_tokens_used = F('openai_title_parser_total_tokens_used') + total_tokens
         self.save()
 
     def reset_title_parser_stats(self):
